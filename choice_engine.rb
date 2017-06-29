09a90829022a1527423f7292968238bf25bdba96
James Jefferies <james@shedcode.co.uk>
2017-06-29 13:10:21 +0100

    WIP

--- choice_engine.rb
+++ choice_engine.rb
@@ -1,8 +1,11 @@
 #!/usr/bin/env ruby
 
 require 'rubygems'
+require 'dotenv'
+
+Dotenv.load
+
 require 'chatterbot/dsl'
-require 'dotenv/load'
 
 #
 # this is the script for the twitter bot ChoiceEngine
@@ -20,38 +23,6 @@ require 'dotenv/load'
 # also has a few settings/directives which you will want to read
 # about, and comment or uncomment as you see fit.
 #
-
-
-#
-# These lines here are just to make sure you edit this file before
-# trying to run your bot. You can safely remove them once you've
-# looked through this file.
-#
-puts "========================"
-puts "========================"
-puts "========================"
-puts "\n\n\n"
-
-puts "Hey there! You should open ChoiceEngine.rb and take a look before running this script."
-
-puts "\n\n\n"
-exit
-
-
-
-#
-# Placing your security credentials right in a script like this is
-# handy, but potentially dangerous, especially if your code is
-# publicly available on github or somewhere else. However, if it is
-# convenient for you to have your authentication data here, you can
-# uncomment the lines below, and copy your configuration data from
-# ChoiceEngine.yml into the commands.
-#
-# consumer_key 'consumer_key'
-# consumer_secret 'consumer_secret'
-#
-# secret 'secret'
-# token 'token'
 
 
 # Enabling **debug_mode** prevents the bot from actually sending
@@ -93,7 +64,7 @@ exclude bad_words
 # This will restrict your bot to tweets that come from accounts that
 # are following your bot. A tweet from an account that isn't following
 # will be rejected
-#only_interact_with_followers
+only_interact_with_followers
 
 #
 # Specifying 'use_streaming' will cause Chatterbot to use Twitter's
@@ -118,23 +89,23 @@ use_streaming
 # Still here? Hopefully it's because you're going to do something cool
 # with the data that doesn't bother other people. Hooray!
 #
-search "alive" do |tweet|
- # here's the content of a tweet
- puts tweets.text
-end
+#search "chatterbot" do |tweet|
+#  # here's the content of a tweet
+#  puts tweets.text
+#end
 
 #
 # this block responds to mentions of your bot
 #
-# puts "checking for replies to my tweets and mentions of me"
-# replies do |tweet|
-#   text = tweet.text
-#   puts "message received: #{text}"
-#   src = text.gsub(/@echoes_bot/, "#USER#")
+replies do |tweet|
+  # replace the incoming username with #USER#, which will be replaced
+  # with the handle of the user who tweeted us by the
+  # replace_variables helper
+  src = tweet.text.gsub(/@ChoiceEngine/, "#USER#")
 
-#   # send it back!
-#   reply src, tweet
-# end
+  # send it back!
+  reply src, tweet
+end
 
 #
 # this block handles incoming Direct Messages. if you want to do
@@ -150,9 +121,9 @@ end
 # (ie, if you were visiting twitter.com) -- using this block might
 # require a little extra work but can be very handy
 #
-home_timeline do |tweet|
- puts tweet.inspect
-end
+# home_timeline do |tweet|
+#  puts tweet.inspect
+# end
 
 #
 # Use this block if you want to be notified about new followers of
