aa5bba754ac8939275d9b781ad435174a0287712
James Jefferies <james@shedcode.co.uk>
2017-07-04 13:14:26 +0100

    TYPO

--- lib/choice_engine/interaction.rb
+++ lib/choice_engine/interaction.rb
@@ -5,8 +5,8 @@ module ChoiceEngine
     belongs_to :post
 
     def self.latest_post_for(username)
-      lastest_posts = where(username: username).order(created_at: :desc).limit(1)
-      lastest_posts.first.post if latest_posts
+      latest_posts = where(username: username).order(created_at: :desc).limit(1)
+      latest_posts.first.post if latest_posts
     end
   end
 end
