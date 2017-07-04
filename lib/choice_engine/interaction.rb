7ed6a93db099989465b2ba86542d96b884827722
James Jefferies <james@shedcode.co.uk>
2017-07-04 13:06:08 +0100

    latest - if there is one

--- lib/choice_engine/interaction.rb
+++ lib/choice_engine/interaction.rb
@@ -5,7 +5,8 @@ module ChoiceEngine
     belongs_to :post
 
     def self.latest_post_for(username)
-      where(username: username).order(created_at: :desc).limit(1).first.post
+      lastest_posts = where(username: username).order(created_at: :desc).limit(1)
+      lastest_posts.first.post if latest_posts
     end
   end
 end
