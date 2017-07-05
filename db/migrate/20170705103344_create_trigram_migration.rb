182d658b969ea536af06b44dab1539f2b729e7c9
James Jefferies <james@shedcode.co.uk>
2017-07-05 10:34:24 +0100

    Add trigram extra

---
+++ db/migrate/20170705103344_create_trigram_migration.rb
@@ -0,0 +1,9 @@
+class CreateTrigramMigration < ActiveRecord::Migration[5.1]
+  def up
+    execute 'CREATE EXTENSION pg_trgm;'
+  end
+
+  def down
+    execute 'DROP EXTENSION pg_trgm;'
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170630151450) do
+ActiveRecord::Schema.define(version: 20170705103344) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+  enable_extension "pg_trgm"
 
   create_table "interactions", force: :cascade do |t|
     t.text "username"
